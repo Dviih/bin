1d81dbeca9aef18954b00bd7c606a1527dffca27
Dviih <dviih2s@gmail.com>
2024-12-14 19:03:28 -0300

    Struct: arrays, maps and slices are handled in the previous case

--- struct.go
+++ struct.go
@@ -133,43 +133,6 @@ func (structs *Struct) ranges(fields map[int]reflect.Value) {
 
 		field = Abs[reflect.Value](field)
 
-		case reflect.Map:
-			if fields[k].Type() == v.Type() {
-				fields[k].Set(v)
-				continue
-			}
-
-			field := fields[k]
-			typ := field.Type()
-
-			m := reflect.MakeMap(reflect.MapOf(typ.Key(), typ.Elem()))
-			r := v.MapRange()
-
-			for r.Next() {
-				key := r.Key()
-				if key.Type() != typ.Key() {
-					if key.Kind() == reflect.Interface && key.Elem().Type() == reflect.TypeFor[*Struct]() {
-						s := key.Interface().(*Struct)
-
-						key = reflect.New(typ.Elem()).Elem()
-						s.ranges(s.fields(key))
-					}
-				}
-
-				value := r.Value()
-				if value.Type() != typ.Elem() {
-					if value.Kind() == reflect.Interface && value.Elem().Type() == reflect.TypeFor[*Struct]() {
-						s := value.Interface().(*Struct)
-
-						value = reflect.New(typ.Elem()).Elem()
-						s.ranges(s.fields(value))
-					}
-				}
-
-				m.SetMapIndex(key, value)
-			}
-
-			fields[k].Set(m)
 		switch field.Kind() {
 		case reflect.Invalid, reflect.Uintptr, reflect.Pointer, reflect.UnsafePointer, reflect.Chan, reflect.Func:
 			continue
@@ -188,38 +151,6 @@ func (structs *Struct) ranges(fields map[int]reflect.Value) {
 				continue
 			}
 
-			Zero(fields[k])
-			s.(*Struct).As(Abs[reflect.Value](fields[k]))
-		case reflect.Array, reflect.Slice:
-			typ := Abs[reflect.Type](fields[k].Type())
-			kind := typ.Kind()
-
-			if kind == reflect.Pointer {
-				kind = Abs[reflect.Type](typ).Kind()
-			}
-
-			field := fields[k]
-			tmp := reflect.New(field.Type()).Elem()
-
-			for i := 0; i < v.Len(); i++ {
-				switch kind {
-				case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128, reflect.Interface, reflect.String:
-					tmp = reflect.Append(tmp, _struct.ptr(v.Index(i), typ))
-				case reflect.Struct:
-					ptr := reflect.New(typ).Elem()
-
-					if s, ok := v.Index(i).Interface().(*Struct); ok {
-						s.As(ptr)
-					}
-
-					tmp = reflect.Append(tmp, ptr)
-				default:
-				}
-			}
-
-			fields[k].Set(tmp)
-		default:
-			fields[k].Set(v.Convert(fields[k].Type()))
 		}
 	}
 }
